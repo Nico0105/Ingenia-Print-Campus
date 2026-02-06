7f8ae9b8d4de30e43a52f52b79ca4bd5109dea8f
Nico0105 <nicogabescolar@gmail.com>
2026-02-06 01:57:03 -0300

    Home changes, Dashboard order changes in the code, and add globe in home.jsx

---
+++ Frontend/src/Components/Globe.jsx
@@ -0,0 +1,17 @@
+<div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
+  <Prism
+    height={5.7}
+    baseWidth={6.5}
+    animationType="rotate"
+    glow={1}
+    noise={0.5}
+    transparent={false}
+    scale={3.6}
+    hueShift={0}
+    colorFrequency={1}
+    hoverStrength={2}
+    inertia={0.05}
+    bloom={1}
+    timeScale={0.5}
+  />
+</div>
