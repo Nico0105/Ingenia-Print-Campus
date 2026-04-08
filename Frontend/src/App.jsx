675ad46ba48c62d99b00555c1ea81b517eb94413
Nicolas Gabriel Escolar <nicogabescolar@gmail.com>
2026-04-08 19:32:58 -0300

    icon

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home.jsx';
 import Curso from './Pages/Curso.jsx';
